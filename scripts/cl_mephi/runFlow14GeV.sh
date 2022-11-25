b9dcd169ed76d8cad4663723f55f5a94596c2a23
DemanovAE <demanov1997@gmail.com>
2022-11-25 15:50:16 +0300

    start scripts changes

--- scripts/cl_mephi/runFlow14GeV.sh
+++ scripts/cl_mephi/runFlow14GeV.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
+#mkdir -p /mnt/pool/rhic/1/demanov/cherenkov/NewSTAR/BES/OUT_new/14GeV/sge_out/
+#mkdir -p /mnt/pool/rhic/1/demanov/cherenkov/NewSTAR/BES/OUT_new/14GeV/sge_err/
+
 #SBATCH -D /mnt/pool/rhic/1/demanov/cherenkov/TMP
 #SBATCH -J flow
-#SBATCH -p compute
 #SBATCH --time=12:00:00
-#SBATCH -a 1-110
-#SBATCH -o /mnt/pool/rhic/1/demanov/cherenkov/NewSTAR/BES/OUT_new/sge_out/slurm_%A_%a.out
-#SBATCH -e /mnt/pool/rhic/1/demanov/cherenkov/NewSTAR/BES/OUT_new/sge_err/slurm_%A_%a.err
+#SBATCH --ntasks=1
+#SBATCH --array=1-110
+#SBATCH -o /mnt/pool/rhic/1/demanov/cherenkov/NewSTAR/BES/OUT_new/14GeV/sge_out/slurm_%A_%a.out
+#SBATCH -e /mnt/pool/rhic/1/demanov/cherenkov/NewSTAR/BES/OUT_new/14GeV/sge_err/slurm_%A_%a.err
 
 PID_TYPE=$1
 WORK_MODE=$2
+IN_REC=$3
+IN_FLAT=$4
+SYF=$5
+
 export ENERGY=14
 export MAIN_DIR=/mnt/pool/rhic/1/demanov/cherenkov/NewSTAR/BES/
 export INPUT=$MAIN_DIR/lists/lists${ENERGY}GeV/StRuns${SLURM_ARRAY_TASK_ID}.list
-export OUTPUT=$MAIN_DIR/OUT_new/${ENERGY}GeV/${ENERGY}GeV_${WORK_MODE}_${PID_TYPE}_${SLURM_ARRAY_JOB_ID}
+export OUTPUT=$MAIN_DIR/OUT/${ENERGY}GeV/${ENERGY}GeV_${WORK_MODE}_${PID_TYPE}_${SYF}_${SLURM_ARRAY_JOB_ID}
 
 source /mnt/pool/rhic/4/parfenovpeter/Soft/Basov/ROOT/build/bin/thisroot.sh
 
-mkdir -p ${MAIN_DIR}/OUT_new/${ENERGY}GeV
-cd ${MAIN_DIR}/OUT_new/${ENERGY}GeV
+mkdir -p ${MAIN_DIR}/OUT/${ENERGY}GeV
+cd ${MAIN_DIR}/OUT/${ENERGY}GeV
  
 mkdir -p $OUTPUT/root
 mkdir -p $OUTPUT/log
 
 cd ${MAIN_DIR}/build/
-./FemtoDstAnalyzer_${PID_TYPE} -i $INPUT -o $OUTPUT/root/JOB_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.root -m ${WORK_MODE} -g ${ENERGY} &>>$OUTPUT/log/JOB_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log
-#./combPID_Gaus -i ${MAIN_DIR}/OUT/27GeV/Histo_27GeVRun10_NewXY.root -f ${MAIN_DIR}/OUT/test.root -o $OUTPUT/root/JOB_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.root -g 27 -m ${WORK_MODE} -p ${SLURM_ARRAY_TASK_ID} &>>$OUTPUT/log/JOB_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log
+./FemtoDstAnalyzer_${PID_TYPE} -i $INPUT -o $OUTPUT/root/JOB_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.root -m ${WORK_MODE} -r ${IN_REC} -f ${IN_FLAT} -g ${ENERGY} &>>$OUTPUT/log/JOB_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log
+#./combPID_Gaus -i ${MAIN_DIR}/OUT/14GeV/Histo_27GeVRun10_NewXY.root -f ${MAIN_DIR}/OUT/test.root -o $OUTPUT/root/JOB_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.root -g 27 -m ${WORK_MODE} -p ${SLURM_ARRAY_TASK_ID} &>>$OUTPUT/log/JOB_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.log
